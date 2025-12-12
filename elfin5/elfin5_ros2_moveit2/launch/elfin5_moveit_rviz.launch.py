e9b861f27dd685f83382c02d5b618b7fdbc8dfde
ogios <ogios@foxmail.com>
2025-12-12 17:41:21 +0800

    add joint limits; remove gazebo related stuff

--- elfin5/elfin5_ros2_moveit2/launch/elfin5_moveit_rviz.launch.py
+++ elfin5/elfin5_ros2_moveit2/launch/elfin5_moveit_rviz.launch.py
@@ -4,56 +4,77 @@
 # Launch file for the elfin5 Robot MoveIt!2 SIMULATION in ROS2 Humble:
 
 # Import libraries:
+from moveit_project.launch_config.xarm.inner import XarmMoveItConfigBuilder
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration,Command, FindExecutable, PathJoinSubstitution
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument,Shutdown
+from launch.substitutions import (
+    LaunchConfiguration,
+    Command,
+    FindExecutable,
+    PathJoinSubstitution,
+)
+from launch.actions import (
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+    DeclareLaunchArgument,
+    Shutdown,
+)
 from launch.conditions import IfCondition, UnlessCondition
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import xacro
 import yaml
 
+
 # LOAD FILE:
 def load_file(package_name, file_path):
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, file_path)
     try:
-        with open(absolute_file_path, 'r') as file:
+        with open(absolute_file_path, "r") as file:
             return file.read()
     except EnvironmentError:
 
         return None
+
+
 # LOAD YAML:
 def load_yaml(package_name, file_path):
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, file_path)
     try:
-        with open(absolute_file_path, 'r') as file:
+        with open(absolute_file_path, "r") as file:
             return yaml.safe_load(file)
     except EnvironmentError:
         return None
 
+
 def generate_launch_description():
 
-    rviz_arg = DeclareLaunchArgument("rviz_file", default_value="False", description="Load RVIZ file.")
-    
-    robot_description_config = (
-        os.path.join(
-            get_package_share_directory("elfin5_ros2_gazebo"),
-            "urdf",
-            "elfin5.urdf.xacro",
-        )
+    rviz_arg = DeclareLaunchArgument(
+        "rviz_file", default_value="False", description="Load RVIZ file."
+    )
+
+    robot_description_config = os.path.join(
+        get_package_share_directory("elfin5_ros2_gazebo"),
+        "urdf",
+        "elfin5.urdf.xacro",
     )
 
     robot_description_config = Command(
-        [FindExecutable(name='xacro'), ' ', robot_description_config,
-        ' use_fake_hardware:=false',
-        ' use_real_hardware:=true',])
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            robot_description_config,
+            " use_fake_hardware:=false",
+            " use_real_hardware:=true",
+        ]
+    )
 
-    robot_description = {'robot_description': robot_description_config}
+    robot_description = {"robot_description": robot_description_config}
 
     # load robot description, srdf
     robot_description_semantic_config = load_file(
@@ -61,28 +82,24 @@ def generate_launch_description():
     )
 
     robot_description_semantic = {
-        "robot_description_semantic":robot_description_semantic_config
+        "robot_description_semantic": robot_description_semantic_config
     }
 
     # load kinematics.yaml
-    kinematics_yaml = load_yaml(
-        "elfin5_ros2_moveit2", "config/kinematics.yaml"
-    )
+    kinematics_yaml = load_yaml("elfin5_ros2_moveit2", "config/kinematics.yaml")
 
-    robot_description_kinematics = {"robot_description_kinematics":kinematics_yaml}
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
 
     # planning functionality
     ompl_planning_pipeline_config = {
-        "move_group":{
-        "planning_plugin": "ompl_interface/OMPLPlanner",
-        "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
-        "start_state_max_bounds_error":0.1,
+        "move_group": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+            "start_state_max_bounds_error": 0.1,
         }
     }
 
-    ompl_planning_yaml = load_yaml(
-        "elfin5_ros2_moveit2","config/ompl_planning.yaml"
-    )
+    ompl_planning_yaml = load_yaml("elfin5_ros2_moveit2", "config/ompl_planning.yaml")
 
     ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
@@ -106,30 +123,55 @@ def generate_launch_description():
     planning_scene_monitor_parameters = {
         "publish_planning_scene": True,
         "publish_geometry_updates": True,
-        "publish_state_updates":  True,
+        "publish_state_updates": True,
         "publish_transforms_updates": True,
+    }
+
+    joint_limits_yaml = {
+        "robot_description_planning": load_yaml(
+            "elfin5_ros2_moveit2", "config/joint_limits.yaml"
+        )
+    }
+
+    a = (
+        XarmMoveItConfigBuilder("a")
+        .planning_pipelines(
+            planner_base_path=get_package_share_directory("moveit_project"),
+            pipelines=["ompl"],
+        )
+        .to_moveit_configs()
+    ).to_dict()
+    planning_pipeline = {
+        "planning_pipelines": a["planning_pipelines"],
+        "default_planning_pipeline": a["default_planning_pipeline"],
+        "ompl": a["ompl"],
     }
 
     # start move_group & action server
     run_move_group_node = Node(
-        package = "moveit_ros_move_group",
-        executable = "move_group",
-        output = "screen",
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
         parameters=[
             robot_description,
             robot_description_semantic,
-            kinematics_yaml,
-            ompl_planning_pipeline_config,
+            # kinematics_yaml,
+            {"robot_description_kinematics": kinematics_yaml},
+            # ompl_planning_pipeline_config,
+            planning_pipeline,
             trajectory_execution,
             moveit_controllers,
             planning_scene_monitor_parameters,
+            joint_limits_yaml,
         ],
     )
 
     # Rviz
     load_rviz = LaunchConfiguration("rviz_file")
-    rviz_base = os.path.join(get_package_share_directory("elfin5_ros2_moveit2"),"launch")
-    rviz_full_config = os.path.join(rviz_base,"elfin5_moveit2.rviz")
+    rviz_base = os.path.join(
+        get_package_share_directory("elfin5_ros2_moveit2"), "launch"
+    )
+    rviz_full_config = os.path.join(rviz_base, "elfin5_moveit2.rviz")
     rviz_node_full = Node(
         package="rviz2",
         executable="rviz2",
@@ -139,8 +181,11 @@ def generate_launch_description():
         parameters=[
             robot_description,
             robot_description_semantic,
-            ompl_planning_pipeline_config,
-            kinematics_yaml,
+            # ompl_planning_pipeline_config,
+            planning_pipeline,
+            # kinematics_yaml,
+            {"robot_description_kinematics": kinematics_yaml},
+            joint_limits_yaml,
         ],
         condition=UnlessCondition(load_rviz),
     )
@@ -151,11 +196,19 @@ def generate_launch_description():
         executable="static_transform_publisher",
         name="static_transform_publisher",
         output="log",
-        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "elfin_base_link"],
-
+        arguments=[
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "0.0",
+            "world",
+            "elfin_base_link",
+        ],
     )
 
-    #publish tf
+    # publish tf
     robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -169,13 +222,16 @@ def generate_launch_description():
         "config",
         "elfin_arm_control.yaml",
     )
-    
-    elfin_drivers_yaml = os.path.join(get_package_share_directory("elfin_robot_bringup"),
-    "config", "elfin_drivers.yaml")
+
+    elfin_drivers_yaml = os.path.join(
+        get_package_share_directory("elfin_robot_bringup"),
+        "config",
+        "elfin_drivers.yaml",
+    )
     ros2_control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description,ros2_controllers_path],
+        parameters=[robot_description, ros2_controllers_path],
         output={
             "stdout": "screen",
             "stderr": "screen",
@@ -200,23 +256,30 @@ def generate_launch_description():
     elfin_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["elfin_arm_controller", "--controller-manager", "/controller_manager"],
+        arguments=[
+            "elfin_arm_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
     )
 
     joint_state_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
     )
 
     return LaunchDescription(
-        [   
+        [
             rviz_arg,
             rviz_node_full,
             run_move_group_node,
             elfin_controller_spawner,
-            joint_state_spawner
-
+            joint_state_spawner,
         ]
         + load_controllers
     )
